e4abe437d71655c785b548062e951a119c6857fa
zidong <1627147430@qq.com>
2023-09-12 09:19:12 +0800

    首次提交

---
+++ src/main/java/com/example/community/dao/DiscussPostMapper.java
@@ -0,0 +1,22 @@
+package com.example.community.dao;
+
+import com.example.community.entity.DiscussPost;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface DiscussPostMapper {
+
+    List<DiscussPost> selectDiscussPosts(int userId, int offset, int limit);
+
+    // @Param用于取别名
+    //如果只有一个参数，并且是动态的，则必须加别名
+    int selectDiscussPostRows(@Param("userId") int userId);
+
+
+
+
+
+}
---
+++ src/test/java/com/example/community/MapperTests.java
@@ -0,0 +1,73 @@
+package com.example.community;
+
+import com.example.community.dao.DiscussPostMapper;
+import com.example.community.dao.UserMapper;
+import com.example.community.entity.DiscussPost;
+import com.example.community.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
+import java.util.List;
+
+@SpringBootTest
+public class MapperTests {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testSelectUser(){
+        User user = userMapper.selectById(101);
+        System.out.println(user);
+
+        User liubei = userMapper.selectByName("liubei");
+        System.out.println(liubei);
+
+        User user1 = userMapper.selectByEmail("nowcoder101@sina.com");
+        System.out.println(user1);
+    }
+
+    @Test
+    public void testInsertUser(){
+        User user = new User();
+        user.setUsername("test");
+        user.setPassword("123456");
+        user.setSalt("abc");
+        user.setEmail("11@qq.com");
+        user.setHeaderUrl("http://www.nowcoder.com/101.png");
+        user.setCreateTime(new Date());
+
+        int rows = userMapper.insertUser(user);
+        System.out.println(rows);
+        System.out.println(user.getId());
+    }
+
+    @Test
+    public void updateUser(){
+        int rows = userMapper.updateStatus(150, 1);
+        System.out.println(rows);
+
+        rows = userMapper.updateHeader(150, "http://www.nowcoder.com/102.png");
+        System.out.println(rows);
+
+        rows = userMapper.updatePassword(150, "hello");
+        System.out.println(rows);
+
+    }
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Test
+    public void testSelectPosts(){
+        List<DiscussPost> discussPosts = discussPostMapper.selectDiscussPosts(149, 0, 10);
+        for (DiscussPost discussPost : discussPosts){
+            System.out.println(discussPost);
+        }
+
+        int rows = discussPostMapper.selectDiscussPostRows(149);
+        System.out.println(rows);
+    }
+}
