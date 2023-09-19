c348fbe0777da2f5dd192362730d892fa2fae07c
zidong <1627147430@qq.com>
2023-09-19 22:08:29 +0800

    第七天，springsecurity实现权限控制和置顶加精删除

---
+++ src/main/java/com/example/community/config/SecurityConfig.java
@@ -0,0 +1,98 @@
+package com.example.community.config;
+
+import com.example.community.util.CommunityConstant;
+import com.example.community.util.CommunityUtil;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements CommunityConstant {
+
+    // 忽略静态资源
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        // 哪些请求需要哪些权限
+        http.authorizeRequests()
+                .antMatchers("/user/setting",
+                        "/user/upload",
+                        "/user/profile/**",
+                        "/discuss/add",
+                        "/comment/add/**",
+                        "/letter/**",
+                        "/notice/**",
+                        "/like",
+                        "/follow",
+                        "/unfollow")
+                .hasAnyAuthority(
+                        AUTHORITY_USER,
+                        AUTHORITY_ADMIN,
+                        AUTHORITY_MODERATOR)
+                .antMatchers(
+                        "/discuss/top",
+                        "/discuss/wonderful"
+                )
+                .hasAnyAuthority(
+                        AUTHORITY_MODERATOR,
+                        AUTHORITY_ADMIN
+                )
+                .antMatchers("/discuss/delete")
+                .hasAnyAuthority(
+                        AUTHORITY_ADMIN
+                )
+                .anyRequest().permitAll().and().csrf().disable();  // 不生成token
+
+
+        // 没登陆的情况
+        http.exceptionHandling()
+                .authenticationEntryPoint(new AuthenticationEntryPoint() {
+                    @Override
+                    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+                        String xRequestWith = httpServletRequest.getHeader("x-requested-with");
+                        // 如果是异步请求
+                        if ("XMLHttpRequest".equals(xRequestWith)) {
+                            httpServletResponse.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = httpServletResponse.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403, "你还没登录哦"));
+                        } else {
+                            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/login");
+                        }
+                    }
+                })
+                .accessDeniedHandler(new AccessDeniedHandler() {
+                    // 权限不足的情况
+                    @Override
+                    public void handle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AccessDeniedException e) throws IOException, ServletException {
+                        String xRequestWith = httpServletRequest.getHeader("x-requested-with");
+                        // 如果是异步请求
+                        if ("XMLHttpRequest".equals(xRequestWith)) {
+                            httpServletResponse.setContentType("application/plain;charset=utf-8");
+                            PrintWriter writer = httpServletResponse.getWriter();
+                            writer.write(CommunityUtil.getJSONString(403, "你没有访问此功能的权限"));
+                        } else {
+                            httpServletResponse.sendRedirect(httpServletRequest.getContextPath() + "/denied");
+                        }
+                    }
+                });
+
+        // 覆盖默认的logout
+        http.logout().logoutUrl("/securitylogout");
+    }
+}
